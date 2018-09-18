e4b5670446d45b16a14405a56b1c5168297a0b1a
TaeHyun KIM <thyun.kim@synctechno.com>
2018-09-18 14:35:52 +0900

    change project name

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
  * Created by kimtaehyun on 2017. 7. 14..
  */
 
-var CiotDatabusClient = require('./lib/ciot.databus.client');
+var CiotDatabusClient = require('./lib/osif.client.js');
 
 exports.Client = CiotDatabusClient;
